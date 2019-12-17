7b6865a1faee12ee4a51c1d541cebaf35bc7fb12
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:03:16 +0300

    Scripted auto-commit on change (2019-12-17 05:03:16) by gitwatch.sh

--- lab5/src/main/java/AkkaStreamsApp.java
+++ lab5/src/main/java/AkkaStreamsApp.java
@@ -4,6 +4,8 @@ public class AkkaStreamsApp {
 
     public static void main(String[] args) throws IOException {
         
+        System.out.println(AkkaStreamsAppConstants.START_MESSAGE);
+
 
     }
 }
