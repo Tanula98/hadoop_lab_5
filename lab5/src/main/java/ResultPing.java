c88e31e21881385536e805708ae7d1e43f4eb08d
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:17:26 +0300

    Scripted auto-commit on change (2019-12-17 05:17:26) by gitwatch.sh

--- lab5/src/main/java/ResultPing.java
+++ lab5/src/main/java/ResultPing.java
@@ -3,5 +3,9 @@ public class ResultPing {
     private String url;
     private Long ping;
 
-    
+
+    public ResultPing(String url, Long ping) {
+        this.url = url;
+        this.ping = ping;
+    }
 }
