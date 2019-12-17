993c18073b0f66df8722453fb748f69c628992e2
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:08:18 +0300

    Scripted auto-commit on change (2019-12-17 05:08:18) by gitwatch.sh

--- lab5/src/main/java/AkkaStreamsApp.java
+++ lab5/src/main/java/AkkaStreamsApp.java
@@ -25,6 +25,7 @@ public class AkkaStreamsApp {
         final Http http = Http.get(system);
         final ActorMaterializer materializer =
                 ActorMaterializer.create(system);
+        
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
         метода которому передаем Http, ActorSystem и ActorMaterializer>;
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
