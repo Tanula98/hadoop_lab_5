084afbc5de53ceec5af3d8cfbdf45c30451faf3c
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:28:00 +0300

    Scripted auto-commit on change (2019-12-17 05:28:00) by gitwatch.sh

--- lab5/src/main/java/TestPing.java
+++ lab5/src/main/java/TestPing.java
@@ -1,2 +1,18 @@
 public class TestPing {
+
+    private String url;
+    private Integer count;
+
+    public TestPing(String url, Integer count) {
+        this.url = url;
+        this.count = count;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
 }
