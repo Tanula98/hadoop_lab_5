2fdddfe25f832caceb439c703e466bb730d0c4d4
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:15:10 +0300

    Scripted auto-commit on change (2019-12-17 05:15:10) by gitwatch.sh

--- lab5/src/main/java/CacheActor.java
+++ lab5/src/main/java/CacheActor.java
@@ -1,6 +1,12 @@
 import akka.actor.AbstractActor;
+import akka.actor.Props;
 
 public class CacheActor extends AbstractActor {
+
+
+    static Props props() {
+        return Props.create(CacheActor.class);
+    }
 
     @Override
     public Receive createReceive() {
