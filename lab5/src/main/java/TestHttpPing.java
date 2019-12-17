28c091f9e311231e08f564df621656f1e6f72843
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:25:50 +0300

    Scripted auto-commit on change (2019-12-17 05:25:50) by gitwatch.sh

--- lab5/src/main/java/TestHttpPing.java
+++ lab5/src/main/java/TestHttpPing.java
@@ -1,7 +1,13 @@
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 
 public class TestHttpPing {
     ActorRef cacheActor;
+
+    public TestHttpPing(ActorSystem system) {
+        cacheActor = system.actorOf(CacheActor.props(), AkkaStreamsAppConstants.CACHE_ACTOR_NAME);
+    }
+
     
 
 }
