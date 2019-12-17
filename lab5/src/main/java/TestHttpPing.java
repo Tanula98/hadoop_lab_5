f1f5f0d13aa36e2906995ff6b46949c03963c6f0
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:26:48 +0300

    Scripted auto-commit on change (2019-12-17 05:26:48) by gitwatch.sh

--- lab5/src/main/java/TestHttpPing.java
+++ lab5/src/main/java/TestHttpPing.java
@@ -1,5 +1,11 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import org.asynchttpclient.AsyncHttpClient;
 
 public class TestHttpPing {
     ActorRef cacheActor;
@@ -8,7 +14,12 @@ public class TestHttpPing {
         cacheActor = system.actorOf(CacheActor.props(), AkkaStreamsAppConstants.CACHE_ACTOR_NAME);
     }
 
-    
+    public Flow<HttpRequest, HttpResponse, NotUsed> createRouteFlow(AsyncHttpClient asyncHttpClient, ActorMaterializer materializer) {
+
+        
+    }
+
+
 
 }
 
