a7ceb31e06166dc048642fb1c904f0628362ff7a
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 06:00:41 +0300

    Scripted auto-commit on change (2019-12-17 06:00:41) by gitwatch.sh

--- lab5/src/main/java/TestHttpPing.java
+++ lab5/src/main/java/TestHttpPing.java
@@ -47,7 +47,7 @@ public class TestHttpPing {
                             .thenCompose(req ->{
                                 ResultPing res = (ResultPing) req;
                                 if (res.getPing() != null){
-                                    return completedFuture.
+                                    return CompletableFuture.completedFuture
                                 } else {
 
                                 }
