4e39895337ad690c6064436be3f04132a1fe1181
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:05:55 +0300

    Scripted auto-commit on change (2019-12-17 05:05:55) by gitwatch.sh

--- lab5/src/main/java/AkkaStreamsApp.java
+++ lab5/src/main/java/AkkaStreamsApp.java
@@ -6,7 +6,24 @@ public class AkkaStreamsApp {
 
         System.out.println(AkkaStreamsAppConstants.START_MESSAGE);
         //а. Инициализация http сервера в akka
-        
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
+        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress
+                RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown
+        when done
 
 
     }
