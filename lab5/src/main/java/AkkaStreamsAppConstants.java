8bc8d0a0091eadf2508a34a5f8eebd911216966b
Tanula98 <tanula98zaa@gmail.com>
2019-12-17 05:11:47 +0300

    Scripted auto-commit on change (2019-12-17 05:11:47) by gitwatch.sh

--- lab5/src/main/java/AkkaStreamsAppConstants.java
+++ lab5/src/main/java/AkkaStreamsAppConstants.java
@@ -4,5 +4,7 @@ public class AkkaStreamsAppConstants {
     static final String START_MESSAGE = "start!";
     static final String START_SERVER_MESSAGE = "Server online at http://localhost:8080/\nPress RETURN to stop...\n";
 
-
+    // Actors Constants
+    static final String ACTOR_SYSTEM_NAME = "routes";
+    static final String CACHE_ACTOR_NAME = "cacheActor";
 }
